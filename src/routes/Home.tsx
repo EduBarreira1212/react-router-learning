f01cc2a3d4ee57491dd8796494517fdb9e41b0fa
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-25 00:20:44 -0300

    feat: styled components created in Home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
+const Div = styled.div`
+    margin-top: 20vh;
+`;
 
-const Home = () => {
-    return <h1>Home</h1>
+const P = styled.p`
+    font-size: larger;
+    font-weight: 600;
+`;
+
+const Button = styled.button`
+    height: 5vh;
+    width: 8vw;
+    font-size: large;
+    font-weight: 500;
+    cursor: pointer;
+`;
+
+const Home: React.FC = () => {
+    const navigate = useNavigate();
+
+    return (
+        <Div>
+            <P>Hello, you need to login</P>
+            <Button onClick={() => navigate("/login")}>Go to Login</Button>
+        </Div>
+    );
 }
 
 export default Home;
