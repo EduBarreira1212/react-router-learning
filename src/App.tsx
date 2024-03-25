3aec8ac870398f2e85789b9dd7db098927ca8134
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-24 22:53:48 -0300

    feat: NavBar and Outlet added to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Outlet } from 'react-router-dom';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
+      <Outlet/>
     </div>
   );
 }
