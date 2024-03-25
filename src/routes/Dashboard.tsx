29c4d1070c5abef5b7c354d3e52bcd6e4bf70bb7
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-25 18:16:49 -0300

    feat: dashboard content created

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,7 +1,12 @@
 
 
 const Dashboard = () => {
-    return <h1>Dashboard</h1>
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <h3>Welcome user</h3>
+        </div>
+    );
 }
 
 export default Dashboard;
