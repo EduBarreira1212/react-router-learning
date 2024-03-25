ea24ef1ba4e2778cb30d33cbd46be5e5cd42502c
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-25 00:22:10 -0300

    feat: link to dashboard removed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,9 +10,6 @@ const NavBar = () => {
                 <li>
                     <Link to={"/login"}>Login</Link>
                 </li>
-                <li>
-                    <Link to={"/dashboard"}>Dashboard</Link>
-                </li>
             </ul>
         </nav>
     );
