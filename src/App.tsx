ae7777355dfa337442ad8a2c1f27b9f274952b51
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-25 00:38:24 -0300

    feat: styled components added to app and css file removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import './App.css';
 import { Outlet } from 'react-router-dom';
 import NavBar from './components/Navbar';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Div>
       <NavBar/>
       <Outlet/>
-    </div>
+    </Div>
   );
 }
 
